699c9a4d3dd8656c340a930271526db7a3ebc0f4
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2021-05-25 12:34:12 +0530

    Added Character Movement

---
+++ Shield Throw/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController cc;
+    public Transform cam;
+    public Animator anim;
+
+    public float movementSpeed = 10f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    void Update()
+    {
+        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            cc.Move(moveDir.normalized * movementSpeed * Time.deltaTime);
+
+            anim.SetBool("Running", true);
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+        }
+    }
+}
