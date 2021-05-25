7cde12a36052d97af00b8d1106feab1242732460
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2021-05-25 21:14:42 +0530

    Added Shield

---
+++ Shield Throw/Assets/Scripts/ShieldScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ShieldScript : MonoBehaviour
+{
+    [SerializeField] Transform shieldTarget;
+
+    private void Update()
+    {
+        transform.localPosition = new Vector3(0.15f, 0f, 0f) + shieldTarget.position;
+        transform.localRotation = Quaternion.Euler(45f, 180f, 0f)*shieldTarget.rotation;
+    }
+}
